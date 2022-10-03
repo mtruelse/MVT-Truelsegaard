c7167563ec90d0f9ee1e5f9d4ce669a89ca60ae9
mtruelse <truelsegaardmariana@gmail.com>
2022-10-03 19:21:07 -0300

    correcciones

--- MVT_project/AppFamiliares/views.py
+++ MVT_project/AppFamiliares/views.py
@@ -4,13 +4,13 @@ from AppFamiliares.models import Familiar
 # Create your views here.
 
 def familiares(request):
-    mama = Familiar(nombre="Marisa", edad="57", fecha_nacimiento="1965-01-25")
+    mama = Familiar(nombre="Juana", edad="50", fecha_nacimiento="1972-03-25")
     mama.save()
 
-    papa = Familiar(nombre="José", edad="63", fecha_nacimiento="1959-06-28")
+    papa = Familiar(nombre="Jorge", edad="60", fecha_nacimiento="1962-05-26")
     papa.save()
 
-    hermano = Familiar(nombre="Martín", edad="32", fecha_nacimiento="1990-01-19")
+    hermano = Familiar(nombre="Roberto", edad="35", fecha_nacimiento="1987-02-19")
     hermano.save()
 
     return render(request, 'template_1.html', {"nombre_madre": mama.nombre, "edad_madre": mama.edad, "nacimiento_madre": mama.fecha_nacimiento, "nombre_padre": papa.nombre, "edad_padre": papa.edad, "nacimiento_padre": papa.fecha_nacimiento, "nombre_hermano": hermano.nombre, "edad_hermano": hermano.edad, "nacimiento_hermano": hermano.fecha_nacimiento})
