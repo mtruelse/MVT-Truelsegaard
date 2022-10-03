3ee1b4bfbc48fb47c3a7257943b33b0cb9497866
mtruelse <truelsegaardmariana@gmail.com>
2022-10-03 19:03:57 -0300

    creacion de los demás familiares

--- MVT_project/AppFamiliares/views.py
+++ MVT_project/AppFamiliares/views.py
@@ -6,4 +6,13 @@ from AppFamiliares.models import Familiar
 def familiares(request):
     mama = Familiar(nombre="Marisa", edad="57", fecha_nacimiento="1965-01-25")
     mama.save()
-    return render(request, 'template_1.html', {"nombre": mama.nombre, "edad": mama.edad, "nacimiento": mama.fecha_nacimiento})
+
+    papa = Familiar(nombre="José", edad="63", fecha_nacimiento="1959-06-28")
+    papa.save()
+
+    hermano = Familiar(nombre="Martín", edad="32", fecha_nacimiento="1990-01-19")
+    hermano.save()
+
+    return render(request, 'template_1.html', {"nombre_madre": mama.nombre, "edad_madre": mama.edad, "nacimiento_madre": mama.fecha_nacimiento, "nombre_padre": papa.nombre, "edad_padre": papa.edad, "nacimiento_padre": papa.fecha_nacimiento, "nombre_hermano": hermano.nombre, "edad_hermano": hermano.edad, "nacimiento_hermano": hermano.fecha_nacimiento})
+
+
